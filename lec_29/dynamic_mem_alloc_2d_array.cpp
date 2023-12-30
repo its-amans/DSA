e4fda22ccf377fc0a3af1c93f88c442d22eac099
honey8899 <amansrivastava8778@gmail.com>
2023-12-31 04:06:32 +0530

    Added program for implementation of a2D array using Dynamic Memory Allocation.(heap memory)

---
+++ lec_29/dynamic_mem_alloc_2d_array.cpp
@@ -0,0 +1,39 @@
+// program for dynamicallly mem allocation of a 2d array
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int row;
+//     int col;
+//     cin>>row;
+//     cin>>col;
+//     // decl
+//     int** arr= new int*[row];
+
+//     // creating a 2d array
+//     for(int i=0; i<row;i++){
+//         arr[i]=new int[col];
+//     }
+
+//     // input values to it
+//     for(int i=0;i<col;i++){
+//         for(int j=0;j<col;j++){
+//             cin >> arr[i][j];
+//         }
+//     }
+    
+
+//     cout<<endl;
+//     // Output the value
+//     for(int i=0;i<row;i++){
+//         for(int j=0;j<col;j++){
+//             cout<<arr[i][j]<<" ";
+//         }
+//         cout<<endl;
+//     }
+
+//     // deleting the m,emory from heap
+//     for(int i=0;i<row;i++){
+//         delete [] arr[i];    // delete array i
+//     }
+// delete []arr;
+// }
