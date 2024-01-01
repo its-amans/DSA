f011608aa8da718dcc6c5190e560ba10dcc82d0e
honey8899 <amansrivastava8778@gmail.com>
2024-01-02 02:23:51 +0530

    lec_31 completed

---
+++ lec_31/recursion.cpp
@@ -0,0 +1,20 @@
+//Fbonacci leetcode.
+#include<iostream>
+using namespace std;
+    int fib(int n) {
+        if(n==0){
+            return 0;
+        }
+        else if(n==1){
+            return 1;
+        }
+        else{
+            return fib(n-1)+fib(n-2);
+        }
+    }
+int main(){
+    int n;
+    cout<<"Enter the term :";
+    cin>>n;
+    cout<<"The nth fib is :"<<fib(n);
+}
