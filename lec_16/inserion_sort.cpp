d15fe87a83e8157d9be773bd239699446d1768ae
honey8899 <amansrivastava8778@gmail.com>
2024-01-01 23:39:14 +0530

    :tada Added some more lectures .. arranged folders and dedleted some redundent files.

--- lec_16/inserion_sort.cpp
+++ lec_16/inserion_sort.cpp
@@ -1,36 +1,67 @@
-// INSERTION_SORT
+// // INSERTION_SORT
+
+// #include<iostream>
+// using namespace std;
+// void insertionsort(int arr[],int n){
+//     for (int i = 1; i < n-1; i++)
+//     {
+//         int  j=i-1;
+//         int temp=arr[i];
+//         for (; j >= 0; j--)
+//         {
+//             if(temp<arr[j])
+//             {
+//                 arr[j+1]=arr[j];
+//             }
+//             else{
+//                 break;
+//             }
+//         }
+//         // the j is taken i-1 i.e the previous element of i and out of the loop of j we have increment j  by 1 to get the correct position of i.if it is printed inside the loop then  arr[j]=temp is printed.
+//         arr[j+1]=temp;
+//     }
+// }
+// void printarray(int arr[], int n){
+//     for(int i=0;i<n;i++){
+//         cout<<arr[i]<<" ";
+//     }   
+// }
+// int main(){
+//     int arr[]={10 ,1 ,7 ,4 ,8 ,2 ,11};
+//     int n=sizeof(arr)/sizeof(arr[0]);
+//     cout<<"THE SORTED ARRAY IS :";
+//     insertionsort(arr ,n);
+//     printarray(arr,n);
+// }
+
+
 
 #include<iostream>
 using namespace std;
 void insertionsort(int arr[],int n){
-    for (int i = 1; i < n-1; i++)
-    {
-        int  j=i-1;
+    for(int i=1;i<n-1;i++){
         int temp=arr[i];
-        for (; j >= 0; j--)
-        {
-            if(temp<arr[j])
-            {
-                arr[j+1]=arr[j];
+        int j=i-1;
+        for(;j>=0;j--){
+            if(temp<arr[j]){
+               arr[j+1]=arr[j];
             }
             else{
                 break;
             }
         }
-        // the j is taken i-1 i.e the previous element of i and out of the loop of j we have increment j  by 1 to get the correct position of i.if it is printed inside the loop then  arr[j]=temp is printed.
         arr[j+1]=temp;
     }
 }
-void printarray(int arr[], int n){
+ void printarray(int arr[], int n){
     for(int i=0;i<n;i++){
         cout<<arr[i]<<" ";
     }   
 }
-int main(){
+ int main(){
     int arr[]={10 ,1 ,7 ,4 ,8 ,2 ,11};
     int n=sizeof(arr)/sizeof(arr[0]);
     cout<<"THE SORTED ARRAY IS :";
     insertionsort(arr ,n);
     printarray(arr,n);
 }
-
--- lec_18/selection_sort.cpp
+++ lec_18/selection_sort.cpp
@@ -1,23 +1,50 @@
-//SELECTION SORT
+// //SELECTION SORT
+
+// #include<iostream>
+// #include<vector>
+// using namespace std;
+// void selectionSort(int arr[], int n)
+// {   
+//     for(int i = 0; i < n-1; i++ ) {
+//         int minIndex = i;
+        
+//         for(int j = i+1; j<n; j++) {
+            
+//             if(arr[j] < arr[minIndex]) 
+//                 minIndex = j;
+            
+//         }
+//         swap(arr[minIndex], arr[i]);
+//     }
+// }
+
+// void printarray(int arr[], int n){
+//     for(int i=0;i<n;i++){
+//         cout<<arr[i]<<" ";
+//     }   
+// }
+// int main(){
+//     int arr[]={10 ,1 ,7 ,4 ,8 ,2 ,11};
+//     int n=sizeof(arr)/sizeof(arr[0]);
+//     cout<<"THE SORTED ARRAY IS :";
+//     selectionSort(arr ,n);
+//     printarray(arr,n);
+// }
+
 
 #include<iostream>
-#include<vector>
 using namespace std;
-void selectionSort(int arr[], int n)
-{   
-    for(int i = 0; i < n-1; i++ ) {
-        int minIndex = i;
-        
-        for(int j = i+1; j<n; j++) {
-            
-            if(arr[j] < arr[minIndex]) 
-                minIndex = j;
-            
+void selectionSort(int arr[],int n){
+    for(int i=0;i<n-1;i++){
+        int minindex=i;
+        for(int j=i+1;j<n;j++){    // i+1 is bcz the the comparision starts ahead of i and before i all are sorted already
+            if(arr[j]<arr[minindex]){
+                minindex=j;
+            }
         }
-        swap(arr[minIndex], arr[i]);
+            swap(arr[minindex],arr[i]);
     }
 }
-
 void printarray(int arr[], int n){
     for(int i=0;i<n;i++){
         cout<<arr[i]<<" ";
